5e78615132ace9fbd86a24e53d48868c7a6ccaf9
Gilberto Garcia <gilbertodamian14@gmail.com>
2020-06-30 14:15:29 -0500

    Fixed [null] when saving the format actions

--- src/main/java/me/fixeddev/ezchat/format/ClickAction.java
+++ src/main/java/me/fixeddev/ezchat/format/ClickAction.java
@@ -2,24 +2,26 @@ package me.fixeddev.ezchat.format;
 
 public enum ClickAction {
     EXECUTE_COMMAND(null),
-    EXECUTE(EXECUTE_COMMAND),
     SUGGEST_COMMAND(null),
-    SUGGEST(SUGGEST_COMMAND),
     OPEN_URL(null),
+    EXECUTE(EXECUTE_COMMAND),
+    SUGGEST(SUGGEST_COMMAND),
     OPEN(OPEN_URL),
     NONE(null);
 
     private ClickAction aliasOf;
     private ClickAction shortVersion;
 
-    ClickAction(ClickAction aliasOf){
-        if(aliasOf == null){
+    ClickAction(ClickAction aliasOf) {
+        if (aliasOf == null) {
             this.aliasOf = this;
         } else {
             this.aliasOf = aliasOf;
-            aliasOf.shortVersion = this;
-            shortVersion = this;
+
+            aliasOf.setShortVersion(this);
         }
+
+        setShortVersion(this);
     }
 
     public ClickAction getAliasOf() {
@@ -30,4 +32,7 @@ public enum ClickAction {
         return shortVersion;
     }
 
+    void setShortVersion(ClickAction shortVer) {
+        shortVersion = shortVer;
+    }
 }
