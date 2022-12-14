69e261362b12eef65af7aa65888dceadd6661a9b
FixedDev <gilbertodamian14@gmail.com>
2022-12-14 17:38:01 -0600

    codestyle: Remove commented unused code.

--- src/main/java/me/fixeddev/ezchat/format/ChatFormatSerializer.java
+++ src/main/java/me/fixeddev/ezchat/format/ChatFormatSerializer.java
@@ -21,40 +21,13 @@ public class ChatFormatSerializer {
     private final LegacyComponentSerializer componentSerializer = LegacyComponentSerializer.builder().hexColors().useUnusualXRepeatedCharacterHexFormat().extractUrls().build();
     private boolean paper = true;
 
-    private EasyChatPartConverter easyChatPartConverter = new EasyChatPartConverter();
+    private final EasyChatPartConverter easyChatPartConverter = new EasyChatPartConverter();
 
     public Component constructJsonMessage(NewChatFormat chatFormat, Player player) {
         return chatFormat.asComponent(easyChatPartConverter.unsafeFunctionForPlayer(player))
                 .replaceText(TextReplacementConfig.builder()
                         .matchLiteral("{displayName}")
                         .replacement(displayName(player)).build());
-        /*
-        Component prefix = fromString(color(chatFormat.getPrefix()));
-
-        if (!chatFormat.getPrefixTooltip().isEmpty()) {
-            prefix = createHover(chatFormat.getPrefixTooltip(), prefix, s -> fromString(replacePlaceholders(player, s)));
-        }
-
-        prefix = setClickAction(chatFormat.getPrefixClickAction(), prefix, replacePlaceholders(player, chatFormat.getPrefixClickActionContent()));
-
-        Component playerName = fromString(color(chatFormat.getPlayerName().replace("{name}", player.getName())));
-        playerName = playerName.replaceText(TextReplacementConfig.builder().matchLiteral("{displayName}").replacement(displayName(player)).build());
-
-        if (!chatFormat.getPlayerNameTooltip().isEmpty()) {
-            playerName = createHover(chatFormat.getPlayerNameTooltip(), playerName, s -> fromString(replacePlaceholders(player, s)));
-        }
-
-        playerName = setClickAction(chatFormat.getPlayerNameClickAction(), playerName, replacePlaceholders(player, chatFormat.getPlayerNameClickActionContent()));
-
-        Component suffix = fromString(color(chatFormat.getSuffix()));
-
-        if (!chatFormat.getSuffixTooltip().isEmpty()) {
-            suffix = createHover(chatFormat.getSuffixTooltip(), suffix, s -> fromString(replacePlaceholders(player, s)));
-        }
-
-        suffix = setClickAction(chatFormat.getSuffixClickAction(), suffix, replacePlaceholders(player, chatFormat.getSuffixClickActionContent()));
-
-        return prefix.append(playerName).append(suffix);*/
     }
 
     public Component constructJsonMessage(NewChatFormat chatFormat, Player player, Audience viewer) {
@@ -62,31 +35,6 @@ public class ChatFormatSerializer {
                 .replaceText(TextReplacementConfig.builder()
                         .matchLiteral("{displayName}")
                         .replacement(displayName(player)).build());
-/*
-        Component prefix = fromString(color(chatFormat.getPrefix()));
-        if (!chatFormat.getPrefixTooltip().isEmpty()) {
-            prefix = createHover(chatFormat.getPrefixTooltip(), prefix, s -> fromString(replacePlaceholders(s, player, viewer)));
-        }
-
-        prefix = setClickAction(chatFormat.getPrefixClickAction(), prefix, replacePlaceholders(chatFormat.getPrefixClickActionContent(), player, viewer));
-
-        Component playerName = fromString(color(chatFormat.getPlayerName().replace("{name}", player.getName())));
-        playerName = playerName.replaceText(TextReplacementConfig.builder().matchLiteral("{displayName}").replacement(displayName(player)).build());
-
-        if (!chatFormat.getPlayerNameTooltip().isEmpty()) {
-            playerName = createHover(chatFormat.getPlayerNameTooltip(), playerName, s -> fromString(replacePlaceholders(s, player, viewer)));
-        }
-
-        playerName = setClickAction(chatFormat.getPlayerNameClickAction(), playerName, replacePlaceholders(chatFormat.getPrefixClickActionContent(), player, viewer));
-
-        Component suffix = fromString(color(chatFormat.getSuffix()));
-
-        if (!chatFormat.getSuffixTooltip().isEmpty()) {
-            suffix = createHover(chatFormat.getSuffixTooltip(), suffix, s -> fromString(replacePlaceholders(s, player, viewer)));
-        }
-
-        suffix = setClickAction(chatFormat.getSuffixClickAction(), suffix, replacePlaceholders(chatFormat.getPrefixClickActionContent(), player, viewer));
-        return prefix.append(playerName).append(suffix);*/
     }
 
     public static String replacePlaceholders(Player player, String message) {
@@ -95,44 +43,6 @@ public class ChatFormatSerializer {
 
     public static String color(String message) {
        return ColorReplacement.color(message);
-    }
-
-    private Component setClickAction(ClickAction action, Component textComponent, String content) {
-        switch (action) {
-            case OPEN_URL:
-                textComponent = textComponent.clickEvent(ClickEvent.openUrl(content));
-                break;
-            case EXECUTE_COMMAND:
-                textComponent = textComponent.clickEvent(ClickEvent.runCommand(content));
-                break;
-            case SUGGEST_COMMAND:
-                textComponent = textComponent.clickEvent(ClickEvent.suggestCommand(content));
-                break;
-            default:
-            case NONE:
-                break;
-        }
-
-        return textComponent;
-    }
-
-    private Component createHover(List<String> hover, Component component, Function<String, Component> converterFunction) {
-        TextComponent hoverComponent = Component.empty();
-        for (int i = 0; i < hover.size(); i++) {
-            String line = color(hover.get(i));
-
-            if (i >= hover.size() - 1) {
-                hoverComponent = hoverComponent.append(converterFunction.apply(line));
-            } else {
-                hoverComponent = hoverComponent.append(converterFunction.apply(line)).append(Component.text('\n'));
-            }
-        }
-
-        return component.hoverEvent(HoverEvent.showText(hoverComponent));
-    }
-
-    private String replacePlaceholders(String message, Player source, Audience viewer) {
-        return PlaceholderReplacer.getInstance().replaceRelational(source, viewer, message);
     }
 
     private Component fromString(String text) {
